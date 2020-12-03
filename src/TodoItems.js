30a42c0f378dd3132eb1448c3f1268483fec7425
Rashmith <rashmiths.191cs242@nitk.edu.in>
2020-12-03 19:54:03 +0530

    implemented flip animation

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import FlipMove from "react-flip-move";
 
 class TodoItems extends Component {
   constructor(props) {
@@ -48,9 +49,7 @@ class TodoItems extends Component {
     var listItems = todoEntries.map(this.createTasks);
     return (
       <ul className="theList">
-        <FlipMove duration={250} easing="ease-out">
-          {listItems}
-        </FlipMove>
+        <FlipMove easing="ease-out">{listItems}</FlipMove>
       </ul>
     );
   }
