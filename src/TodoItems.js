0e428fe842493b0fc71c5f6519ef19c15f345a33
Rashmith <rashmiths.191cs242@nitk.edu.in>
2020-12-03 19:19:09 +0530

    modified look of todo items

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -6,12 +6,42 @@ class TodoItems extends Component {
 
     this.createTasks = this.createTasks.bind(this);
   }
+  getStyle = (completed) => {
+    return {
+      background: "#F4F4F4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: completed ? "line-through" : "none",
+    };
+  };
   createTasks(item) {
     return (
-      <li onClick={() => this.delete(item.key)} key={item.key}>
-        {item.text}
-      </li>
+      <div style={this.getStyle(item.completed)}>
+        <p>
+          <input
+            type="checkbox"
+            onChange={() => this.mark(item.key)}
+            checked={item.completed ? "checked" : ""}
+          />{" "}
+          {item.text}
+          <button
+            onClick={() => this.delete(item.key)}
+            key={item.key}
+            style={{ float: "right" }}
+          >
+            <i class="fa fa-trash" aria-hidden="true">
+              Delete
+            </i>
+          </button>
+        </p>
+      </div>
     );
+  }
+  delete(key) {
+    this.props.delete(key);
+  }
+  mark(key) {
+    this.props.markComplete(key);
   }
   render() {
     var todoEntries = this.props.entries;
